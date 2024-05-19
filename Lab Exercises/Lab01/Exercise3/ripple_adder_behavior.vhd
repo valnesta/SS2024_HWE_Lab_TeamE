cafb29ca85be2b27d43460fdf68add20e56cd3ae
onyesi <115337792+onyesi@users.noreply.github.com>
2024-05-19 19:59:53 +0200

    Update ripple_adder_behavior.vhd

--- Lab Exercises/Lab01/Exercise3/ripple_adder_behavior.vhd
+++ Lab Exercises/Lab01/Exercise3/ripple_adder_behavior.vhd
@@ -1,66 +1,51 @@
-ENTITY Tb_Ripple_Adder IS
-END Tb_Ripple_Adder;
- 
-ARCHITECTURE behavior OF Tb_Ripple_Adder IS
- 
--- Component Declaration for the Unit Under Test (UUT)
- 
-COMPONENT Ripple_Adder
-PORT(
-A : IN std_logic_vector(3 downto 0);
-B : IN std_logic_vector(3 downto 0);
-Cin : IN std_logic;
-S : OUT std_logic_vector(3 downto 0);
-Cout : OUT std_logic
-);
-END COMPONENT;
- 
---Inputs
-signal A : std_logic_vector(3 downto 0) := (others => '0');
-signal B : std_logic_vector(3 downto 0) := (others => '0');
-signal Cin : std_logic := '0';
- 
---Outputs
-signal S : std_logic_vector(3 downto 0);
-signal Cout : std_logic;
- 
-BEGIN
- 
--- Instantiate the Unit Under Test (UUT)
-uut: Ripple_Adder PORT MAP (
-A => A,
-B => B,
-Cin => Cin,
-S => S,
-Cout =>; Cout
-);
- 
--- Stimulus process
-stim_proc: process
+entity ripple_adder is
+    port(
+        A, B: in std_logic_vector(3 downto 0);
+        Cin: in std_logic;
+        Sum_out: out std_logic_vector(3 downto 0);
+        Carry_out: out std_logic
+    );
+end ripple_adder;
+
+architecture behavior of ripple_adder is
+    signal c: std_logic_vector(3 downto 1); -- Internal carry signals
+
+    component full_adder
+        port(
+            A, B, Cin: in std_logic;
+            Sum, Cout: out std_logic
+        );
+    end component;
 begin
--- hold reset state for 100 ns.
-wait for 100 ns;
-A <= "0110";
-B <= "1100";
- 
-wait for 100 ns;
-A <= "1111";
-B <= "1100";
- 
-wait for 100 ns;
-A <= "0110";
-B <= "0111";
- 
-wait for 100 ns;
-A <= "0110";
-B <= "1110";
- 
-wait for 100 ns;
-A <= "1111";
-B <= "1111";
- 
-wait;
- 
-end process;
- 
-END;
+    FA0: full_adder port map (
+        A => A(0),
+        B => B(0),
+        Cin => Cin,
+        Sum => Sum_out(0),
+        Cout => c(1)
+    );
+
+    FA1: full_adder port map (
+        A => A(1),
+        B => B(1),
+        Cin => c(1),
+        Sum => Sum_out(1),
+        Cout => c(2)
+    );
+
+    FA2: full_adder port map (
+        A => A(2),
+        B => B(2),
+        Cin => c(2),
+        Sum => Sum_out(2),
+        Cout => c(3)
+    );
+
+    FA3: full_adder port map (
+        A => A(3),
+        B => B(3),
+        Cin => c(3),
+        Sum => Sum_out(3),
+        Cout => Carry_out
+    );
+end behavior;
