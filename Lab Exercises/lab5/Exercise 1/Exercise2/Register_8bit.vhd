11316aaba6da86347497c4a8c51073b6e30b485e
onyesi <115337792+onyesi@users.noreply.github.com>
2024-07-27 07:54:36 +0100

    Update Register_8bit.vhd

--- Lab Exercises/lab5/Exercise 1/Exercise2/Register_8bit.vhd
+++ Lab Exercises/lab5/Exercise 1/Exercise2/Register_8bit.vhd
@@ -1,1 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Register_8bit is
+    Port ( D : in STD_LOGIC_VECTOR(7 downto 0);
+           CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR(7 downto 0));
+end Register_8bit;
+
+architecture Structural of Register_8bit is
+    component D_FlipFlop
+        Port ( D : in STD_LOGIC;
+               CLK : in STD_LOGIC;
+               RESET : in STD_LOGIC;
+               Q : out STD_LOGIC);
+    end component;
+begin
+    GEN_REG: for i in 0 to 7 generate
+        DFF: D_FlipFlop 
+           port map (
+                D => D(i),
+                CLK => CLK,
+                RESET => RESET,
+                Q => Q(i)
+              );
+    end generate GEN_REG;
+end Structural;
 
